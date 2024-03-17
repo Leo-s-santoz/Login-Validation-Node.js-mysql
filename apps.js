04f35b5b1b067ee0dca58ecad2640251e3498ec5
Leonardo <leonardosantos11080@gmail.com>
2024-03-17 20:18:28 -0300

    conectado_ao_bdd

--- apps.js
+++ apps.js
@@ -7,4 +7,16 @@ app.get("/", function(req, res){
     res.sendFile(__dirname+"/cadastro/index.html");
 });
 
+const Sequelize = require("sequelize")
+const sequelize = new Sequelize("sistemaDeCadastro", "root", "#Leo_5921", {
+    host: "localhost",
+    dialect: "mysql"
+})
+
+sequelize.authenticate().then(function(){
+    console.log("Conectado")
+}).catch(function(erro){
+    console.log("Falha ao conectar: "+erro)
+})
+
 app.listen(8081, function(){console.log("servidor rodando");});
